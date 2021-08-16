7beea42eee125c76306c7361b288c222c6b7ddc9
Gelin Eguinosa <eguinosa@gmail.com>
2021-08-16 13:41:50 -0400

    Add static method to create Tokenizer from saved tokens.

--- corpus_tokenizer.py
+++ corpus_tokenizer.py
@@ -1,7 +1,8 @@
 # Gelin Eguinosa Rosique
 
 import pickle
-from os.path import isfile, join
+from os import mkdir
+from os.path import isdir, isfile, join
 
 from docs_tokenization import lazy_corpus_tokenization
 
@@ -11,8 +12,13 @@ class CorpusTokenizer:
     Class to tokenize the documents of a corpus and save the results in case
     they are needed later.
     """
+    # Save the location of the tokens for static methods.
+    tokens_folder = 'data/corpus_tokens'
 
-    def __init__(self, documents, use_saved=False):
+    # Save index name for static methods.
+    index_name = 'index.pickle'
+
+    def __init__(self, documents, _use_saved=False):
         """
         Receives the texts from the documents in the corpus and creates, and
         transforms each document into an array of tokens.
@@ -20,18 +26,27 @@ class CorpusTokenizer:
         documents, lowercases the text and lemmatizes each token.
         :param documents: An iterable sequence containing the texts of the
         documents in the corpus.
-        :param use_saved: Bool to determine if we used a previously
+        :param _use_saved: Bool to determine if we used a previously
         calculated tokenization of the corpus, or if we start from scratch, even
         if we have the result of the tokenization saved.
         """
+        # The Location of the data of the program
+        self._data_folder = 'data'
+        # Create data folder if it doesn't exist
+        if not isdir(self._data_folder):
+            mkdir(self._data_folder)
+
         # The Location of the Tokens
-        self._tokens_folder = 'data/corpus_tokens'
+        self._tokens_folder = join(self._data_folder, 'corpus_tokens')
+        # Create the Tokens folder if it doesn't exist
+        if not isdir(self._tokens_folder):
+            mkdir(self._tokens_folder)
 
         # The location of the index of the tokens
         self._index_name = 'index.pickle'
 
         # Check if the user wants to use the saved tokens
-        if use_saved:
+        if _use_saved:
             index_path = join(self._tokens_folder, self._index_name)
             # Check if we saved the tokens for this corpus
             if not isfile(index_path):
@@ -77,3 +92,38 @@ class CorpusTokenizer:
             with open(tokens_path, 'rb') as file:
                 doc_tokens = pickle.load(file)
             yield doc_tokens
+
+    @staticmethod
+    def saved_tokens():
+        """
+        Check if the tokens from a previos tokenizer are saved and ready to
+        be used.
+        :return: bool representing if we have the tokens or the corpus saved or
+        not.
+        """
+        tokens_folder = CorpusTokenizer.tokens_folder
+        index_name = CorpusTokenizer.index_name
+        index_path = join(tokens_folder, index_name)
+
+        # Check that the index file exists.
+        if not isfile(index_path):
+            return False
+
+        # Open the index file and check there is information available
+        with open(index_path, 'rb') as file:
+            tokens_info = pickle.load(file)
+
+        # Check if token_info has null values or is empty
+        if not tokens_info or not len(tokens_info):
+            return False
+
+        return True
+
+    @classmethod
+    def saved_tokenizer(cls):
+        """
+        Creates a CorpusTokenizer from the information saved by a previous
+        tokenizer.
+        :return: A CorpusTokenizer
+        """
+        return cls(None, _use_saved=True)
