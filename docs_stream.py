1d4ab784fb4c7b2f0304708c0c5ec1f05b7fa03b
Gelin Eguinosa <eguinosa@gmail.com>
2021-08-20 20:55:48 -0400

    Check if there are documents available before loading them.

--- docs_stream.py
+++ docs_stream.py
@@ -1,30 +1,37 @@
 # Gelin Eguinosa Rosique
 
 from os import listdir
-from os.path import isfile, join
+from os.path import isdir, isfile, join
 
 
 class DocumentsManager:
     """Class to manage the files of the documents in the corpus."""
 
-    def __init__(self, dir_path='docs', file_ext=''):
+    # The location of the documents
+    docs_folder = 'docs'
+    # The file extension of the documents
+    docs_suffix = '.txt'
+
+    def __init__(self):
         """
         Receives the path where all the documents of the corpus are, and creates
         a dictionary with the names of the files and their locations.
-        :param dir_path: The directory where the documents of the corpus are
-        located.
-        :param file_ext: The file extension of the documents belonging to the
-        corpus.
         """
+        # Check if the docs directory exists:
+        if not isdir(self.docs_folder):
+            raise Exception(f"The folder '{self.docs_folder}', where the"
+                            f" documents are supposed to be located, does not"
+                            f" exist.")
+
         # Creating dictionary where all the documents and their locations will
         # be saved.
         self.documents = {}
 
         # Iterate through all the documents in the given directory and save the
         # locations of the documents.
-        for doc_name in listdir(dir_path):
-            doc_path = join(dir_path, doc_name)
-            if isfile(doc_path) and doc_name.endswith(file_ext):
+        for doc_name in listdir(self.docs_folder):
+            doc_path = join(self.docs_folder, doc_name)
+            if isfile(doc_path) and doc_name.endswith(self.docs_suffix):
                 self.documents[doc_name] = doc_path
 
     def documents_texts(self):
@@ -33,8 +40,31 @@ class DocumentsManager:
         :return: A sequence containing the texts all the documents in the
         corpus.
         """
+        # Iterate through the locations of the documents saved in the index.
         for file_path in self.documents.values():
             document = open(file_path)
             text_document = document.read()
             document.close()
             yield text_document
+
+    @classmethod
+    def documents_available(cls):
+        """
+        Checks if the class has any documents to work with in the designated
+        folder.
+        :return: A bool representing if there are documents available to work
+        with or not.
+        """
+        # Check if the docs directory exists.
+        if not isdir(cls.docs_folder):
+            return False
+
+        # Check if there is any document inside docs folder.
+        for doc_name in listdir(cls.docs_folder):
+            doc_path = join(cls.docs_folder, doc_name)
+            if isfile(doc_path) and doc_name.endswith(cls.docs_suffix):
+                # There is at least one document available
+                return True
+
+        # Return false, if we couldn't find any document in the folder
+        return False
